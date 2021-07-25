874d838e4fddf51771ba01af6b14f3314ccd491f
Fabian Mathews <supagu@gmail.com>
2021-07-25 19:51:42 +0930

    experimenting with secondary tileset of image collections

---
+++ assets/props.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="props" tilewidth="197" tileheight="329" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="65" height="109" source="images/tree_1.png"/>
+ </tile>
+</tileset>
